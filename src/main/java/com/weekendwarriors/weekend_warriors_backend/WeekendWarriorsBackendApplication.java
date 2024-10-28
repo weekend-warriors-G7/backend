c6a9b912aec8b0498d3353b906b456f344966b46
Gabjea <gabjea856@gmail.com>
2024-10-28 13:12:54 +0200

    Create a GET method to test the app

--- src/main/java/com/weekendwarriors/weekend_warriors_backend/WeekendWarriorsBackendApplication.java
+++ src/main/java/com/weekendwarriors/weekend_warriors_backend/WeekendWarriorsBackendApplication.java
@@ -2,9 +2,17 @@ package com.weekendwarriors.weekend_warriors_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WeekendWarriorsBackendApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Weekend Warriors Backend: Greetings from Spring Boot Dockerized!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeekendWarriorsBackendApplication.class, args);
