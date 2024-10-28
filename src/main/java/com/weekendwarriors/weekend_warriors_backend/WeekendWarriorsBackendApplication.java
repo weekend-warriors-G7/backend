a4fb5c0bf69478b1738cd40c4e98d3a112f3beef
Gabjea <gabjea856@gmail.com>
2024-10-28 08:54:48 +0200

    update project dependencies

--- src/main/java/com/weekendwarriors/weekend_warriors_backend/WeekendWarriorsBackendApplication.java
+++ src/main/java/com/weekendwarriors/weekend_warriors_backend/WeekendWarriorsBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeekendWarriorsApplication {
+public class WeekendWarriorsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeekendWarriorsApplication.class, args);
+		SpringApplication.run(WeekendWarriorsBackendApplication.class, args);
 	}
 
 }
